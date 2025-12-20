413860360c52108329ed47f51a81becf7c14672c
Deyvid0809 <davi25090@gmail.com>
2025-12-20 07:42:21 -0300

    Opção nome com letras maiúsculas adicionada

--- Missao4.java
+++ Missao4.java
@@ -5,18 +5,26 @@ public class Missao4 {
         Scanner sc = new Scanner(System.in);
 
         int op = 0;
-        while (op != 3) {
+        while (op != 4) {
         System.out.println("Escolha uma opção:");
         System.out.println("1 - Dizer Ola");
         System.out.println("2 - Mostrar uma frase");
-        System.out.println("3 - sair");
+        System.out.println("3 - Digitar nome e mostrar em maiúsculo");
+        System.out.println("4 - sair");
         op = sc.nextInt();
         
         if (op == 1) {
             System.out.println("Olá, seja bem-vindo!");
         }else if (op == 2) {
             System.out.println("Você está aprendendo Java");
-        }else if (op == 3) {
+        }else if(op == 3){
+            System.out.println("Me diga seu nome:");
+            String nome = sc.next();
+            nome = nome.toUpperCase();
+            System.err.printf("Seu nome com letras Maiúsculas: %s", nome);
+            System.err.println();
+        } 
+        else if (op == 4) {
             System.out.println("Encerrando programa!!");
         } else{
         System.out.println("Opção inválida");}
