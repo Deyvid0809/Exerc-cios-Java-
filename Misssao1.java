5b36d4482f30b39a37ca55c9531663a611281c39
Deyvid0809 <davi25090@gmail.com>
2025-12-20 06:19:03 -0300

    Media

--- Misssao1.java
+++ Misssao1.java
@@ -8,9 +8,21 @@ public class Misssao1{
         System.out.println("Quantos números você quer digita:");
         int quantNumero = sc.nextInt();
 
+        int menor = 0;
         for (int i = 1; i <= quantNumero; i++) {
+
+            System.out.print("Digite um numero:");
             int numero = sc.nextInt();
+            if (i == 1) {
+                menor = numero;
+            }else if (numero <= menor) {
+                menor = numero;
+            }
         }
+
+        System.err.printf("O menor numero: %d", menor);
+
+        sc.close();
 
     }
 }
---
+++ Misssao2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Misssao2 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Quantos número você quer saber a média:");
+        int quantNumero = sc.nextInt();
+
+        double media = 0;
+        for (int i = 1; i <= quantNumero; i++) {
+
+            System.out.print("Digite um numero pra calcular sua média:");
+            double pergunmedia = sc.nextDouble();
+
+            media += pergunmedia;
+            
+        }
+
+        double resulMedia = media/quantNumero;
+        System.out.printf("Média: %.2f%n", resulMedia);
+    }
+}
