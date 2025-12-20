69c66a7cfa03d3824702936cdbaf3d94c1fa83da
Deyvid0809 <davi25090@gmail.com>
2025-12-20 05:18:02 -0300

    MENOR número

---
+++ Misssao1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Misssao1{
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Quantos números você quer digita:");
+        int quantNumero = sc.nextInt();
+
+        for (int i = 1; i <= quantNumero; i++) {
+            int numero = sc.nextInt();
+        }
+
+    }
+}
