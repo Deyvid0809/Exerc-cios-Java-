518f402c3578c61509286fdeb706ca14e2fe268c
Deyvid0809 <davi25090@gmail.com>
2025-12-20 06:47:35 -0300

    Contador de notas

--- Misssao2.java
+++ Misssao2.java
@@ -5,7 +5,7 @@ public class Misssao2 {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Quantos número você quer saber a média:");
-        double quantNumero = sc.nextInt();
+        int quantNumero = sc.nextInt();
 
         double media = 0;
         for (int i = 1; i <= quantNumero; i++) {
@@ -15,5 +15,30 @@ public class Misssao2 {
         }
         double resulMedia = media/quantNumero;
         System.out.printf("Média: %.2f%n", resulMedia);
+
+        int acima, igua, abaixo;
+        acima = 0;
+        abaixo = 0;
+        igua = 0;
+        for (int i = 1; i <= quantNumero; i++) {
+            System.out.print("Digite um numero pra calcular sua média:");
+            double pergunmedia = sc.nextDouble();
+            
+            if (pergunmedia > resulMedia) {
+                System.out.print("Acima de média");
+                System.out.println();
+                acima += 1;
+            }else if (pergunmedia == resulMedia) {
+                System.out.print("Igua há média");
+                System.out.println();
+                igua += 1;
+            }else {
+                System.out.print("Abaixo da média");
+                System.out.println();
+                abaixo += 1;
+            }
+        }
+        System.out.printf("Foram acima da média: %d, Foram igua há média: %d, Foram menor que a média: %d", acima, igua, abaixo);
+        sc.close(); 
     }
 }
